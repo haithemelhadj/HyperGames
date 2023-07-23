0f7d7b05248bb48d568847ff5ec9d4c00677d8d9
Elhaj Elhaithem <101217966+haithemelhadj@users.noreply.github.com>
2023-07-23 21:56:58 +0100

    commit

---
+++ Assets/Haithem/Board.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    public GameObject CoffePrefab;
+    //private BackgroundTile[,] allTiles;
+
+
+    void Start()
+    {
+        //allTiles= new BackgroundTile[width, height];
+        SetUp();
+    }
+
+
+    
+
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {        
+            for (int j = 0; j < height; j++)
+            {
+                //spawn backgroundTiles
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity); 
+                //backgroundTile.transform.parent = this.transform; backgroundTile.name = "( " + i + ", " + j + " )";
+                //spawn objects
+                GameObject CofeeType = Instantiate(CoffePrefab, tempPosition,Quaternion.identity);
+                //backgroundTile.transform.parent = this.transform; backgroundTile.name = "( " + i + ", " + j + " )";
+            }
+        }
+    }
+}
