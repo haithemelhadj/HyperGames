e941052f72e10cddf3a4427d9807587ec133c5c0
Elhaj Elhaithem <101217966+haithemelhadj@users.noreply.github.com>
2023-09-06 21:15:40 +0100

    commit

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,19 @@
+using TMPro.Examples;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    public Text ScoreText;
+    public int Score;
     public void LoadScene(string sceneName)
     {
+        Debug.Log("Going to " + sceneName);
         SceneManager.LoadScene(sceneName);
+        if(sceneName=="HomeMenu")
+        {
+            ScoreText.text = "Points:" + Score+50f;
+        }
     }
 }
