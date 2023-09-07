b7b4102976c638da76e138ac7a152635578b9534
Elhaj Elhaithem <101217966+haithemelhadj@users.noreply.github.com>
2023-09-07 23:52:00 +0100

    fixed system points
    if app is closed all progress is lost, needs to be fixed

---
+++ Assets/PointsManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour
+{
+    public Text ScoreText;
+    //public Text Points;
+    public static int UserPoints;
+    //public PlayerPrefs userPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //UserPoints=PlayerPrefs.GetInt("UserPoints");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("prefs : " + PlayerPrefs.GetInt("UserPoints"));
+        Debug.Log("userP : " + UserPoints);
+        ScoreText.text = "Points: " + UserPoints;
+        //PlayerPrefs.SetInt("userPoints", PointsManager.UserPoints);
+
+    }
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,15 +5,21 @@ using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
-    public Text ScoreText;
+    
     public int Score;
+    public int addPoints = 10;
+    
     public void LoadScene(string sceneName)
     {
         Debug.Log("Going to " + sceneName);
         SceneManager.LoadScene(sceneName);
-        if(sceneName=="HomeMenu")
+        if ((sceneName == "GuessThe Dress" || sceneName == "CofeeCrash" || sceneName == "NujaFruit"))// && ScoreText != null) 
         {
-            ScoreText.text = "Points:" + Score+50f;
+            Debug.Log("adding points");
+            PointsManager.UserPoints = PointsManager.UserPoints + addPoints;
+            
+            //Debug.Log(PointsManager.UserPoints);
+            //Debug.Log("playerprefs = "+PlayerPrefs.GetInt("userPoints"));
         }
     }
 }
